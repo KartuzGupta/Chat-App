54b0a0fc1294474c11d8f1ab0c6eacfc9ae14736
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-14 21:15:55 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Homepage from "./Pages/Homepage";
 import { Route } from "react-router-dom";
 import Chatpage from "./Pages/Chatpage";
+import signupui from "./components/UI/signupui";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Homepage} exact />
+      <Route path="/" component={signupui} exact />
       <Route path="/chats" component={Chatpage} />
+      <Route path="/sign" component={Chatpage} />
     </div>
   );
 }
